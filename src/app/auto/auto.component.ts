dbd0dea166d97f303aa12b0008c43f15ac06bccc
corey redding and julian flood <corey+julian@>
2017-07-10 11:52:47 -0700

    get list to appear in auto

--- src/app/auto/auto.component.ts
+++ src/app/auto/auto.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-auto',
   templateUrl: './auto.component.html',
   styleUrls: ['./auto.component.css']
 })
+
 export class AutoComponent {
+  @Input() childAutoList: Post[];
+  selectedPost = null;
+
+  posts: Post[] = [
+    new Post('2004 Ford Escape', 'January 27th', 'clean title and less than 200,000 miles!', 'David@davidsemail.com'),
+    new Post('2016 Volvo C-32', 'October 31st', 'Fast as hell and a cute little hatchback!', 'EdwardCullen@vampire.net')
+  ];
 
 
+  editButtonHasBeenClicked(postToEdit: Post) {
+    this.clickSender.emit(postToEdit);
+  }
 }
--- src/app/post.model.ts
+++ src/app/post.model.ts
@@ -1,2 +1,7 @@
 export class Post {
+  constructor(
+    public title: string,
+    public date: string,
+    public description: string,
+    public contact: string) { }
 }
